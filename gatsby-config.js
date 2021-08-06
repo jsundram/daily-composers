eaa85d9fe94cd3484a09810c08d6d80f43be79a3
Jason Sundram <jsundram@gmail.com>
2021-08-06 15:38:58 -0400

    explore gatsby image plugin options

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,7 +8,24 @@ module.exports = {
   plugins: [
       "gatsby-plugin-gatsby-cloud",
       "gatsby-plugin-image",
-      "gatsby-plugin-sharp",
+      {
+          resolve: `gatsby-plugin-sharp`,
+          options: {
+              defaults: {
+                  formats: [`auto`, `webp`],
+                  placeholder: `dominantColor`,
+                  quality: 50,
+                  breakpoints: [750, 1080, 1366, 1920],
+                  backgroundColor: `transparent`,
+                  tracedSVGOptions: {},
+                  blurredOptions: {},
+                  jpgOptions: {},
+                  pngOptions: {},
+                  webpOptions: {},
+                  avifOptions: {},
+              }
+          }
+      },
       {
           resolve: "gatsby-source-filesystem",
           options: {
