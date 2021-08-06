1341836f04c69dc2a118ec182a26ea2549f7d3c1
Jason Sundram <jsundram@gmail.com>
2021-08-06 15:44:19 -0400

    move css into its own file; mobile friendliness

--- src/templates/day.js
+++ src/templates/day.js
@@ -6,29 +6,15 @@ import { StaticImage } from 'gatsby-plugin-image'
 import Layout from '../components/layout'
 import Composer from '../components/composer'
 
+import {
+    container,
+    heading,
+    button,
+    link,
+} from './day.module.css'
 
-// import next_button from "../images/next-button.svg"
-// import prev_button from "../images/previous-button.svg"
 
-const container = {
-    "display": "flex",
-    "height": "120px",
-    "width": "600px",
-}
-
-const heading = {
-  color: "cornflowerblue",
-  margin: "auto",
-}
-const button = {
-  margin: "auto",
-}
 const button_height = 30;
-
-const link = {
-    color: "cornflowerblue",
-}
-
 
 // TODO: this should probably go someplace more central and get used by
 // gatsby-node.js
@@ -50,8 +36,8 @@ export default function Day({ pageContext }) {
   return (
         <Layout pageTitle={date_str}>
 
-            <div style={container}>
-            <Link style={button} to={dateslug(yesterday)} title="yesterday">
+            <div className={container}>
+            <Link className={button} to={dateslug(yesterday)} title="yesterday">
                 <StaticImage
                     alt="yesterday"
                     src="../images/previous-button.svg"
@@ -60,9 +46,9 @@ export default function Day({ pageContext }) {
                 />
             </Link>
 
-            <h1 style={heading}>{date_str}</h1>
+            <h1 className={heading}>{date_str}</h1>
 
-            <Link style={button} to={dateslug(tomorrow)} title="tomorrow">
+            <Link className={button} to={dateslug(tomorrow)} title="tomorrow">
                 <StaticImage
                     alt="tomorrow"
                     src="../images/next-button.svg"
@@ -74,21 +60,21 @@ export default function Day({ pageContext }) {
 
 
           <h3>
-            Playlist status: {playlist.status || "None (yet!)"}
+            Playlist status: <span style={{fontWeight: 'normal'}}>{playlist.status || "None (yet!)"}</span>
           </h3>
           <ul>
             {
                 playlist_types.map(type => (
                     playlist[type] ?
                     <li key={type}>
-                        <a key={type} href={playlist[type]} style={link}>{type}</a>
+                        <a key={type} href={playlist[type]} className={link}>{type}</a>
                     </li>
                     :
                     null
                 ))
             }
           </ul>
-          <h3>Featured composers for {date_str}</h3>
+          <h3>{group.totalCount} Featured composers for {date_str}:</h3>
           <ul>
           {
             group.nodes
